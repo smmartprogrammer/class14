f24c9a8c2723da352c7b66710d2f708abb86393b
smmartprogrammer <partabrajp@gmail.com>
2021-04-07 21:36:51 +0500

    typscript vs javascript difference to understand

--- src/sample.ts
+++ src/sample.ts
@@ -1,1 +1,15 @@
 console.log("Hello world from TS");
+// let a = 1;
+// a = true;
+// a = [1,2,3];
+
+// let std = {} 
+// console.log(std.name)
+
+// function a(b: number) {
+//     return b /2
+// }
+
+// a("z")
+
+// basic typescript and javascript example
